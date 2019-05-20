0336695b55174f8664074e3db85a1cf3df298596
jyh <875134940@qq.com>
2019-05-20 17:50:20 +0800

    0520

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-# from django.urls import include
+from django.urls import include
 from polls import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('polls/', views.index)
-    #path('polls/', include('polls.urls'))
+    #path('polls/', views.index)
+    path('polls/', include('polls.urls'))
 ]
