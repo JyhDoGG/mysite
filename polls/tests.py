d8fb55d2561576deacf9cd21cbb307a4246ace56
jyh <875134940@qq.com>
2019-05-22 15:05:19 +0800

    0522-1505

--- polls/tests.py
+++ polls/tests.py
@@ -10,3 +10,13 @@ class QuestionMethodTests(TestCase):
         time = timezone.now() + datetime.timedelta(days=30)
         fq = Question(pub_date=time)
         self.assertIs(fq.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_question(self):
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        oq = Question(pub_date=time)
+        self.assertIs(oq.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        rq = Question(pub_date=time)
+        self.assertIs(rq.was_published_recently(), True)
