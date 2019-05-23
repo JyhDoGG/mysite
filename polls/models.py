d351bca89571721ece6e8708adfa591e126cdac1
jyh <875134940@qq.com>
2019-05-24 00:16:01 +0800

    0523

--- polls/models.py
+++ polls/models.py
@@ -24,3 +24,23 @@ class Choice(models.Model):
         return self.choice_text
 
 
+class Person(models.Model):
+    name = models.CharField(max_length=50)
+
+class Group(models.Model):
+    name = models.CharField(max_length=128)
+    members = models.ManyToManyField(
+    Person,
+    through='Membership',
+    through_fields=('group', 'person'),
+    )
+
+class Membership(models.Model):
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    inviter = models.ForeignKey(
+    Person,
+    on_delete=models.CASCADE,
+    related_name="membership_invites",
+    )
+    invite_reason = models.CharField(max_length=64)
